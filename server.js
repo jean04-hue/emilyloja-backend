9bc4ef7445826dc0f14824a7c1115a2acae4f4f2
Jean Schneider <jschneider@Proturbo1.com.br>
2025-10-07 12:05:20 -0300

    Adicionando express como dependência

--- server.js
+++ server.js
@@ -11,10 +11,6 @@ app.use('/auth', require('./routes/auth'));
 app.use('/products', require('./routes/products'));
 app.use('/orders', require('./routes/orders'));
 
-// Rota raiz (importante para Railway)
-app.get('/', (req, res) => {
-  res.send('🚀 API da EmilyLoja está online!');
-});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`✅ Servidor rodando na porta ${PORT}`));
