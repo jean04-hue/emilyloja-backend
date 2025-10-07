482b585ab60efbb6940bdf88613b5f10b3796543
Jean Schneider <jschneider@Proturbo1.com.br>
2025-10-07 12:03:33 -0300

    Adicionada rota raiz para teste no Railway

--- server.js
+++ server.js
@@ -17,4 +17,4 @@ app.get('/', (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(` Servidor rodando na porta ${PORT}`));
+app.listen(PORT, () => console.log(`✅ Servidor rodando na porta ${PORT}`));
